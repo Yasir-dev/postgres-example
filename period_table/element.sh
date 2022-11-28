89beeed73cdfe7a8c1013dffce909a7067766a6e
Yasir Khurshid <41165103+Yasir-dev@users.noreply.github.com>
2022-11-29 00:09:35 +0100

    Update element.sh

--- period_table/element.sh
+++ period_table/element.sh
@@ -7,31 +7,24 @@ then
   exit 0;
 fi
 
-ELEMENT_INFO_RESULT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1");
-
+if [[ ! $1 =~ ^[0-9]+$ ]]
+then
+  ELEMENT_INFO_RESULT=$($PSQL "SELECT * FROM elements WHERE symbol = '$1' OR name = '$1'");
+else
+  ELEMENT_INFO_RESULT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1");
+fi
 
 if [[ -z $ELEMENT_INFO_RESULT ]]
 then
-  ELEMENT_INFO_RESULT=$($PSQL "SELECT * FROM elements WHERE symbol = '$1'");
-  if [[ -z $ELEMENT_INFO_RESULT ]]
-  then
-    ELEMENT_INFO_RESULT=$($PSQL "SELECT * FROM elements WHERE name = '$1'");
-    if [[ -z $ELEMENT_INFO_RESULT ]]
-    then
-    echo "I could not find that element in the database."
-    fi
-  fi
+  echo "I could not find that element in the database."
 else
    echo "$ELEMENT_INFO_RESULT" | while read ATOMIC_ID BAR SYMBOL BAR NAME
-   
    do
-   PROPERTY_RESULT=$($PSQL "SELECT * FROM properties WHERE atomic_number = $ATOMIC_ID");
-   echo "$PROPERTY_RESULT" | while read ATOMIC_ID BAR ATOMIC_MASS BAR MP_C BAR BP_C BAR TYPE_ID
-   do
-   TYPE_RESULT=$($PSQL "SELECT type FROM types WHERE type_id = $TYPE_ID");
-    echo "The element with atomic number $ATOMIC_ID is $NAME ($SYMBOL). It's a $(echo $TYPE_RESULT | sed -r 's/^ *| *$//g'), with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MP_C celsius and a boiling point of $BP_C celsius."
+    PROPERTY_RESULT=$($PSQL "SELECT * FROM properties WHERE atomic_number = $ATOMIC_ID");
+    echo "$PROPERTY_RESULT" | while read ATOMIC_ID BAR ATOMIC_MASS BAR MP_C BAR BP_C BAR TYPE_ID
+    do
+      TYPE_RESULT=$($PSQL "SELECT type FROM types WHERE type_id = $TYPE_ID");
+      echo "The element with atomic number $ATOMIC_ID is $NAME ($SYMBOL). It's a $(echo $TYPE_RESULT | sed -r 's/^ *| *$//g'), with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MP_C celsius and a boiling point of $BP_C celsius."
+    done
    done
-  
-   done
-   
-fi
+fi  
