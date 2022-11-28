06b98386fe62b7c359cea7764e387f90c17548aa
Yasir Khurshid <41165103+Yasir-dev@users.noreply.github.com>
2022-11-28 23:42:28 +0100

    Update element.sh

--- period_table/element.sh
+++ period_table/element.sh
@@ -7,20 +7,31 @@ then
   exit 0;
 fi
 
-ATOMIC_ID_RESULT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1");
+ELEMENT_INFO_RESULT=$($PSQL "SELECT * FROM elements WHERE atomic_number = $1");
 
 
-if [[ -z $ATOMIC_ID_RESULT ]]
+if [[ -z $ELEMENT_INFO_RESULT ]]
 then
-  ATOMIC_SYMBOL_RESULT=$($PSQL "SELECT * FROM elements WHERE symbol = '$1'");
-  if [[ -z $ATOMIC_SYMBOL_ID_RESULT ]]
+  ELEMENT_INFO_RESULT=$($PSQL "SELECT * FROM elements WHERE symbol = '$1'");
+  if [[ -z $ELEMENT_INFO_RESULT ]]
   then
-    ATOMIC_NAME_RESULT=$($PSQL "SELECT * FROM elements WHERE name = '$1'");
-    if [[ -z $ATOMIC_NUMBER_RESULT ]]
+    ELEMENT_INFO_RESULT=$($PSQL "SELECT * FROM elements WHERE name = '$1'");
+    if [[ -z $ELEMENT_INFO_RESULT ]]
     then
     echo "I could not find that element in the database."
     fi
   fi
 else
-   echo "found"
+   echo "$ELEMENT_INFO_RESULT" | while read ATOMIC_ID BAR SYMBOL BAR NAME
+   
+   do
+   PROPERTY_RESULT=$($PSQL "SELECT * FROM properties WHERE atomic_number = $ATOMIC_ID");
+   echo "$PROPERTY_RESULT" | while read ATOMIC_ID BAR ATOMIC_MASS BAR MP_C BAR BP_C BAR TYPE_ID
+   do
+   TYPE_RESULT=$($PSQL "SELECT type FROM types WHERE type_id = $TYPE_ID");
+    echo "The element with atomic number $ATOMIC_ID is $NAME ($SYMBOL). It's a $(echo $TYPE_RESULT | sed -r 's/^ *| *$//g'), with a mass of $ATOMIC_MASS amu. $NAME has a melting point of $MP_C celsius and a boiling point of $BP_C celsius."
+   done
+  
+   done
+   
 fi
